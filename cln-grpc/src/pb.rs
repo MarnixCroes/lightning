e02fdf57a33176b78e2543f45c377ac4aaf6b0db
Christian Decker <decker.christian@gmail.com>
2022-01-27 15:23:46 +0100

    cln-grpc: Add conversion from pb Amount to json Amount

--- cln-grpc/src/pb.rs
+++ cln-grpc/src/pb.rs
@@ -20,3 +20,20 @@ impl From<JAmount> for Amount {
         }
     }
 }
+
+impl From<&Amount> for JAmount {
+    fn from(a: &Amount) -> Self {
+        match a {
+            Amount {
+                unit: Some(amount::Unit::Millisatoshi(v)),
+            } => JAmount::Millisatoshi(*v),
+            Amount {
+                unit: Some(amount::Unit::Satoshi(v)),
+            } => JAmount::Satoshi(*v),
+            Amount {
+                unit: Some(amount::Unit::Bitcoin(v)),
+            } => JAmount::Bitcoin(*v),
+	    o => panic!("Unhandled conversion from pb:Amount to json:Amount: {:?}", o),
+        }
+    }
+}
